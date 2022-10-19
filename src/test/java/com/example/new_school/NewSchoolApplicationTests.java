2c4bc27bacd00bebf545da56187dbe41ec860a93
dimka <dimka411841994@gmail.com>
2022-10-19 20:10:28 +0300

    Initial commit

---
+++ src/test/java/com/example/new_school/NewSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
